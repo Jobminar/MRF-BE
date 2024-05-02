a509df1b86e4060407a4610e1b4ecc2a62b92862
sameerg1810 <64128604+sameerg1810@users.noreply.github.com>
2024-05-03 01:04:02 +0530

    changrd the adminControllrt

--- controller/adminController.js
+++ controller/adminController.js
@@ -1,6 +1,7 @@
 import Admin from "../model/adminModel.js";
 import jwt from "jsonwebtoken";
 import argon2 from "argon2";
+
 const adminController = {
   signup: async (req, res) => {
     try {
@@ -9,22 +10,24 @@ const adminController = {
       if (!phone || !password) {
         return res
           .status(400)
-          .json({ message: "Required fields is missing !!" });
+          .json({ message: "Phone and password are required" });
       }
 
       const existingUser = await Admin.findOne({ phone });
       if (existingUser) {
-        return res.status(400).json({ message: "Already existing admin" });
+        return res.status(400).json({ message: "Admin already exists" });
       }
-      const hashedPassword = await argon2.hash(password, 10);
+
+      const hashedPassword = await argon2.hash(password);
       const newAdmin = new Admin({ phone, password: hashedPassword });
       await newAdmin.save();
-      res.status(201).json({ message: "signup admin successfully" });
+      res.status(201).json({ message: "Admin signup successful" });
     } catch (error) {
-      console.log(error);
-      res.status(500).json({ error: "Failed to admin signup" });
+      console.error(error);
+      res.status(500).json({ error: "Failed to signup admin" });
     }
   },
+
   login: async (req, res) => {
     try {
       const { phone, password } = req.body;
@@ -32,31 +35,32 @@ const adminController = {
       if (!phone || !password) {
         return res
           .status(400)
-          .json({ message: "Required fields is missing !! " });
+          .json({ message: "Phone and password are required" });
       }
 
       const admin = await Admin.findOne({ phone });
       if (!admin) {
-        return res.status(404).json({ message: "Admin not found !!" });
+        return res.status(404).json({ message: "Admin not found" });
       }
 
-      const isValidPassword = await argon2.verify(password, admin.password);
+      const isValidPassword = await argon2.verify(admin.password, password);
       if (!isValidPassword) {
-        return res.status(401).json({ message: "Invalid password " });
+        return res.status(401).json({ message: "Invalid password" });
       }
 
       const token = jwt.sign({ userId: admin._id }, process.env.JWT_SECRET, {
         expiresIn: "24h",
       });
       res.status(200).json({
-        message: "Login admin successfully",
+        message: "Admin login successful",
         adminId: admin._id,
         token,
       });
     } catch (error) {
-      console.log(error);
+      console.error(error);
       res.status(500).json({ error: "Failed to login" });
     }
   },
 };
+
 export default adminController;

7042a10bb195049a120a6f79c1049b983af42709
sameerg1810 <64128604+sameerg1810@users.noreply.github.com>
2024-05-04 15:39:55 +0530

    corrected server.js

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-import router from './routes/index.js'
-import cors from 'cors'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import router from "./routes/index.js";
+import cors from "cors";
 
-dotenv.config()
+dotenv.config();
 
-const app=express()
-const PORT=process.env.PORT || 4000
-app.use(cors())
-app.use(express.json({limit:"30mb"}))
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json({ limit: "30mb" }));
 
+app.use("/", router);
 
-app.use('/',router)
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDb connected successfully"))
+  .catch((error) => console.log("Failed to connect mongoDB", error));
 
-
-mongoose.connect(process.env.MONGO_URI)
-.then(()=>console.log("MongoDb connected successfully"))
-.catch((error)=>console.log("Failed to connect mongoDB",error))
-
-app.listen(PORT,console.log(`server running ${PORT}`))
+app.listen(PORT, console.log(`server running ${PORT}`));
